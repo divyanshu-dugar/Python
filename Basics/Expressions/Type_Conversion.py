c0e57b9b2e1b1897a2a04050105c24a929b587e8
Divyanshu Dugar <divyu2004@gmail.com>
2024-03-25 23:01:05 -0400

    Changed the directory name

---
+++ Basics/Expressions/Type_Conversion.py
@@ -0,0 +1,11 @@
+# Python inputs everything as a string
+old_age = input("Enter your old age : ")
+new_age = int(old_age) + 2  # type conversion
+print(new_age)
+
+'''
+Other type conversions :
+float()
+str()
+bool()
+'''
